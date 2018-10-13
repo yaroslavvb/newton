b6a0b10cd0b678a9657e1fccdb5ca8ca96c093e4
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-10-13 12:19:27 -0700

    nsync and nconnect utilities

---
+++ nconnect.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# connects to instance
+
+import argparse
+import fcntl
+import logging
+import os
+import select
+import subprocess
+import sys
+
+from ncluster import aws_util as u
+
+parser = argparse.ArgumentParser(description='sync')
+parser.add_argument('-v', '--verbose', action='count', dest='verbosity',
+                    default=0, help='Set verbosity.')
+parser.add_argument('-n', '--name', type=str, default='', help="name of instance to sync with")
+args = parser.parse_args()
+
+
+def main():
+  instances = u.lookup_instances(args.name, verbose=False)
+  if not instances:
+    assert False, f"Couldn't find any instances containing {args.name}"
+  instance = instances[0]
+  print(f"Connecting to {u.get_name(instances[0])}")
+  cmd = f"ssh -i {u.get_keypair_fn()} -o StrictHostKeyChecking=no ubuntu@{instance.public_ip_address}"
+  print(cmd)
+  os.system(cmd)
+
+if __name__ == '__main__':
+  sys.exit(main())
