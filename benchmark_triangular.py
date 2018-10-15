00b8b195194ecc74b77373774fe1854844c89312
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-10-15 15:34:13 -0700

    update benchmark numbers

--- benchmark_triangular.py
+++ benchmark_triangular.py
@@ -5,11 +5,18 @@ On p3.2xlarge
 Benchmarking n=8000
 numpy
 Times: min: 127.86, median: 128.12, mean: 128.31
-tensorflow
+
+tensorflow gpu
 Times: min: 1.20, median: 1.20, mean: 1.20
-pytorch
+
+tensorflow cpu
+Times: min: 32.77, median: 32.88, mean: 32.91
+
+pytorch cpu
 Times: min: 162.65, median: 162.65, mean: 162.65
 
+pytorch gpu
+Times: min: 0.04, median: 0.05, mean: 0.05
 
 """
 
@@ -24,7 +31,7 @@ if __name__ == '__main__':
 
   u.reset_timeit()
 
-  iters = 1
+  iters = 3
   n = 8000
 
   print(f"Benchmarking n={n}")
@@ -38,9 +45,24 @@ if __name__ == '__main__':
   b = scipy.randn(n)
 
   u.reset_timeit()
-  for i in range(10):
+  for i in range(iters):
     with u.timeit('numpy'):
       scipy.linalg.solve_triangular(A, b)
+
+  ############################################################
+  # PyTorch
+  ############################################################
+  A = torch.randn(n, n)
+  A = A @ A.t()+torch.diag(torch.ones(n))
+  A = torch.potrf(A).cuda()
+  b = torch.randn(n, 1).cuda()
+
+  # prewarm
+  torch.trtrs(b, A)
+  for i in range(iters):
+    with u.timeit('pytorch'):
+      result = torch.trtrs(b, A)
+    del result
 
   ############################################################
   # Tensorflow
@@ -53,7 +75,7 @@ if __name__ == '__main__':
   # Internal: failed to query device pointer for context: CUDA_ERROR_INVALID_VALUE
   #  A = tf.constant(A).gpu()
   #  b = tf.constant(b).gpu()
-  assert 'gpu' in A.device.lower()
+  #  assert 'gpu' in A.device.lower()
 
   # prewarm
   result =  tf.contrib.eager.Variable(tf.zeros((n, 1)))
@@ -62,19 +84,6 @@ if __name__ == '__main__':
     b+=1  # prevent caching
     with u.timeit('tensorflow'):
       result.assign(tf.linalg.triangular_solve(A, b))
-
-  ############################################################
-  # PyTorch
-  ############################################################
-  A = torch.randn(n, n)
-  b = torch.randn(n, 1)
-  A = A @ A.t()+torch.diag(torch.ones(n))
-  A = torch.potrf(A)
-
-  # prewarm
-  torch.trtrs(b, A)
-  for i in range(iters):
-    with u.timeit('pytorch'):
-      torch.trtrs(b, A)
+    print(result[0,0])
 
   u.summarize_timeit()
